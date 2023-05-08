be623f282ecc63d22e134614b6d390a124ca0e60
daveS <davesimpcontrol@gmail.com>
2023-05-08 15:57:06 -0600

    with assistance from kyle  s. it was brought to my attention that i over complicated the task, and the work to generate the file could be easily consolidated after the prompts

--- index.js
+++ index.js
@@ -1,114 +1,72 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license !== 'N/A') {
-    return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
-  } else {
-    return '';
-  }
-}
-
-// Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license !== 'N/A') {
-    return `- [License](#license)`;
-  } else {
-    return '';
-  }
-}
-
-// Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license !== 'N/A') {
-    return `## License
-
-This project is licensed under the ${license} license.`;
-  } else {
-    return '';
-  }
-}
-
-const inquirer = require('inquirer');
+const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
   return inquirer.prompt([
     {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?'
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "username",
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'Please provide a description of your project.'
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
     },
     {
-      type: 'input',
-      name: 'installation',
-      message: 'What are the installation instructions for your project?'
+      type: "input",
+      message: "What is the name of your project?",
+      name: "title",
     },
     {
-      type: 'input',
-      name: 'usage',
-      message: 'How is your project intended to be used?'
+      type: "input",
+      message: "Please write a short description of your project:",
+      name: "description",
     },
     {
-      type: 'input',
-      name: 'contributing',
-      message: 'How can users contribute to your project?'
+      type: "list",
+      message: "What kind of license should your project have?",
+      name: "license",
+      choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "Unlicense", "None"],
     },
     {
-      type: 'input',
-      name: 'tests',
-      message: 'What are the testing instructions for your project?'
+      type: "input",
+      message: "What command should be run to install dependencies?",
+      name: "installation",
+      default: "npm install",
     },
     {
-      type: 'list',
-      name: 'license',
-      message: 'Which license does your project use?',
-      choices: ['MIT', 'GPLv3', 'Apache', 'BSD', 'N/A']
+      type: "input",
+      message: "What command should be run to run tests?",
+      name: "tests",
+      default: "npm test",
     },
     {
-      type: 'input',
-      name: 'github',
-      message: 'What is your GitHub username?'
+      type: "input",
+      message: "What does the user need to know about using the repo?",
+      name: "usage",
     },
     {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email address?'
-    }
+      type: "input",
+      message: "What does the user need to know about contributing to the repo?",
+      name: "contributing",
+    },
   ]);
 }
 
-
-// Create a function to write the README file
-function writeToFile(fileName, data) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(fileName, data, err => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve('README.md');
-      }
-    });
-  });
+async function init() {
+  try {
+    const answers = await promptUser();
+    const markdown = generateMarkdown(answers);
+    await writeFileAsync("README.md", markdown);
+    console.log("Successfully wrote to README.md");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-// Create a function to initialize the program
-function init() {
-  promptUser()
-    .then(data => generateMarkdown(data, renderLicenseBadge, renderLicenseLink, renderLicenseSection))
-    .then(readme => writeToFile('README.md', readme))
-    .then(fileName => console.log(`Successfully wrote ${fileName}`))
-    .catch(err => console.error(err));
-}
-
-// Call the init function to start the program
 init();
